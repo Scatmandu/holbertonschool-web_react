63c0174210f94c7d9b8f30c669fe1b0beaf17531
Scatmandu <mcclur05@nsuok.edu>
2022-06-01 20:36:42 -0500

    added everything so far

---
+++ 0x00-react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,14 @@
+export const getFullYear = () => {
+  return new Date().getFullYear();
+};
+
+export const getFooterCopy = (isIndex) => {
+  if (isIndex === true) {
+    return 'Holberton School';
+  } else {
+    return 'Holberton School main dashboard'
+  }
+};
+
+export const getLatestNotification = () =>
+  '<strong>Urgent requirement</strong> - complete by EOD';
